b0604266d77e1f8322c8ac3ec7919ca1e55c68fe
indraseptama <indra.septama@ecomindo.com>
2021-12-14 10:14:23 +0700

    feat: add pwa configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
   reactStrictMode: true,
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
   images: {
     domains: ["raw.githubusercontent.com", "example2.com"],
   },
-};
+});
