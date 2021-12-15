d976531b5d882687e4ba0fae0e49084a039d4347
indraseptama <indra.septama@ecomindo.com>
2021-12-15 14:30:46 +0700

    test: render error in pokemon detail

--- components/PokemonDetail/__test__/index.test.jsx
+++ components/PokemonDetail/__test__/index.test.jsx
@@ -1,4 +1,4 @@
-import { render, waitFor, screen } from "@testing-library/react";
+import { render, waitFor, screen, fireEvent } from "@testing-library/react";
 import { MockedProvider } from "@apollo/client/testing";
 import PokemonDetail from "../view";
 import { GET_POKEMON_DETAIL } from "../../../graphql/pokemon-query";
@@ -16,6 +16,22 @@ const mocks = [
     result: {
       data: {
         pokemon: dataDummyPokemon,
+      },
+    },
+  },
+];
+
+const mocksError = [
+  {
+    request: {
+      query: GET_POKEMON_DETAIL,
+      variables: {
+        name: "ivysaur",
+      },
+    },
+    result: {
+      data: {
+        pokemon: { ...dataDummyPokemon, name: null },
       },
     },
   },
@@ -45,4 +61,33 @@ it("renders loading", () => {
   );
 
   expect(getByText("Loading...")).toBeInTheDocument();
+});
+
+it("renders modal pop up", async () => {
+  const addPokemon = jest.fn();
+  const { getByText } = render(
+    <PokemonContext.Provider value={addPokemon}>
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <PokemonDetail name={"ivysaur"} />
+      </MockedProvider>
+    </PokemonContext.Provider>
+  );
+  await waitFor(() => screen.getByText("Skills"));
+  const button = getByText("Catch!");
+  fireEvent.click(button);
+  await waitFor(() => screen.getAllByRole("dialog"));
+  //expect(getByText("ivysaur")).toBeInTheDocument();
+});
+
+it("renders with error", async () => {
+  const addPokemon = jest.fn();
+  const { getByText } = render(
+    <PokemonContext.Provider value={addPokemon}>
+      <MockedProvider mocks={mocksError} addTypename={false}>
+        <PokemonDetail name={"ivysaur"} />
+      </MockedProvider>
+    </PokemonContext.Provider>
+  );
+  await waitFor(() => screen.getByText("Pokemon not found :("));
+  expect(getByText("Pokemon not found :(")).toBeInTheDocument();
 });
