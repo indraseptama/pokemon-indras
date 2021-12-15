e883480a8a6ca5ff15cc1d6f8b1835cbcc6a3c67
indraseptama <indra.septama@ecomindo.com>
2021-12-15 11:23:53 +0700

    fix: fill datadummy for mocking

--- utils/dataDummy.ts
+++ utils/dataDummy.ts
@@ -1,4 +1,5 @@
 import { IPokemon } from "../interface/IPokemon";
+import { IPokemonItem } from "../interface/IPokemonItem";
 
 export const dataDummyPokemon: IPokemon = {
   name: "ivysaur",
@@ -403,4 +404,77 @@ export const dataDummyPokemon: IPokemon = {
   ],
 };
 
-const dataDummyList = [];
+const dataDummyList: Array<IPokemonItem> = [
+  {
+    name: "bulbasaur",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+    id: 1,
+  },
+  {
+    name: "ivysaur",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
+    id: 2,
+  },
+  {
+    name: "venusaur",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
+    id: 3,
+  },
+  {
+    name: "charmander",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+    id: 4,
+  },
+  {
+    name: "charmeleon",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png",
+    id: 5,
+  },
+  {
+    name: "charizard",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png",
+    id: 6,
+  },
+  {
+    name: "squirtle",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
+    id: 7,
+  },
+  {
+    name: "wartortle",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png",
+    id: 8,
+  },
+  {
+    name: "blastoise",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png",
+    id: 9,
+  },
+  {
+    name: "caterpie",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png",
+    id: 10,
+  },
+  {
+    name: "metapod",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/11.png",
+    id: 11,
+  },
+  {
+    name: "butterfree",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/12.png",
+    id: 12,
+  },
+];
