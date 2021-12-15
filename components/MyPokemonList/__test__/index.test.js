73e431eb39e2dfbf399eeb40f4216543586fdd7b
indraseptama <indra.septama@ecomindo.com>
2021-12-15 14:11:43 +0700

    test: add test modal pop up my pokemon list

--- components/MyPokemonList/__test__/index.test.js
+++ components/MyPokemonList/__test__/index.test.js
@@ -1,4 +1,11 @@
-import { render, waitFor, screen, act } from "@testing-library/react";
+import {
+  render,
+  waitFor,
+  screen,
+  act,
+  getAllByText,
+  fireEvent,
+} from "@testing-library/react";
 
 import MyPokemonList from "../view";
 import { dummyMyPokemons } from "../../../utils/dataDummy";
@@ -17,5 +24,31 @@ it("renders without error", async () => {
     </PokemonContext.Provider>
   );
   await waitFor(() => screen.getByText("You have 12 pokemons"));
+  expect(getByText("charmander")).toBeInTheDocument();
+});
+
+it("renders pop up modal", async () => {
+  const countPokemons = 12;
+  const myPokemons = dummyMyPokemons;
+  const removePokemon = jest.fn();
+  const { container, getByText } = render(
+    <PokemonContext.Provider
+      value={{ countPokemons, myPokemons, removePokemon }}
+    >
+      <MyPokemonList />
+    </PokemonContext.Provider>
+  );
+
+  await waitFor(() => screen.getByText("You have 12 pokemons"));
+  const buttonRelease = getAllByText(container, "Release");
+  fireEvent.click(buttonRelease[0]);
+  expect(getByText("Are you sure to release Hellow?")).toBeInTheDocument();
+  const buttonRemove = getByText("Remove");
+  fireEvent.click(buttonRemove);
+  fireEvent.click(buttonRelease[0]);
+  const buttonCancel = getByText("Cancel");
+  fireEvent.click(buttonCancel);
+  fireEvent.click(buttonRelease[0]);
+  fireEvent.keyDown(window, { key: "Escape", code: "Escape" });
   expect(getByText("charmander")).toBeInTheDocument();
 });
